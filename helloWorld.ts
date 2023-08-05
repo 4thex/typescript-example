7665dd198fd2bd9c222887997a59b1537010e4b5
Palle Cogburn <palle@4thex.com>
2023-08-05 20:48:48 +0200

    Making it work with package.json imports property

--- helloWorld.ts
+++ helloWorld.ts
@@ -1,5 +1,4 @@
-import 'module-alias/register';
-import { convert } from 'X/converter';
+import { convert } from '#X/converter';
 let message: string = 'Hello World';
 
 console.log(convert(message));
